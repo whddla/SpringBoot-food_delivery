159102b08edbddd222aad73b5d6386fe2a81acd6
whddla <whddla8912@gmail.com>
2022-12-09 09:29:08 +0900

    Controller 추가
    Controller 추가

---
+++ src/main/java/com/example/food_delivery/domain/controller/AdminController.java
@@ -0,0 +1,8 @@
+package com.example.food_delivery.domain.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+public class AdminController {
+}
---
+++ src/main/java/com/example/food_delivery/domain/controller/StoreController.java
@@ -0,0 +1,12 @@
+package com.example.food_delivery.domain.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+//@RestController api 확인시 기존 Conroller 어노테이션 주석하고 이거 활성
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class StoreController {
+}
