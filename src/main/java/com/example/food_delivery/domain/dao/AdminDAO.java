420593cf9094835e9711ad1b7bb98a852666f5a3
whddla <whddla8912@gmail.com>
2022-12-09 10:03:21 +0900

    DAO
    DAO

---
+++ src/main/java/com/example/food_delivery/domain/dao/AdminDAO.java
@@ -0,0 +1,4 @@
+package com.example.food_delivery.domain.dao;
+
+public class AdminDAO {
+}
---
+++ src/main/java/com/example/food_delivery/domain/dao/StoreDAO.java
@@ -0,0 +1,8 @@
+package com.example.food_delivery.domain.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+@Repository
+@RequiredArgsConstructor
+public class StoreDAO {
+}
