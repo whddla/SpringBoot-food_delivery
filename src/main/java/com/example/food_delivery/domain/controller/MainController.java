6e0f7b9363fe929b338f9f3b903f106b5b967d3e
whddla <whddla8912@gmail.com>
2022-12-09 10:31:50 +0900

    메인페이지
    메인페이지

--- src/main/java/com/example/food_delivery/domain/controller/MainController.java
+++ src/main/java/com/example/food_delivery/domain/controller/MainController.java
@@ -3,10 +3,16 @@ package com.example.food_delivery.domain.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class MainController {
-
+    
+    //메인 페이지
+    @GetMapping("")
+    public String index(){
+        return "index";
+    }
 }
