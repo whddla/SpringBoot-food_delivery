41a737a9d36a53ef02784f56c5337d8dad3dc401
whddla <whddla8912@gmail.com>
2022-12-09 10:06:15 +0900

    MainController
    MainController

---
+++ src/main/java/com/example/food_delivery/domain/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.food_delivery.domain.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class MainController {
+
+}
