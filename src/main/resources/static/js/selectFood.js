811bd3bcde06ee9c0a8f9f1bc592671a98483bd8
whddla <whddla8912@gmail.com>
2022-12-13 17:53:37 +0900

    결제 금액 수정
    결제 금액 수정

--- src/main/resources/static/js/selectFood.js
+++ src/main/resources/static/js/selectFood.js
@@ -1,40 +1,45 @@
 function select(id) {
     var name = id
     var money = document.getElementsByName(id)[0].innerText
+    var pMoney = document.getElementById("pay_food")
+    var tMoney = document.getElementById("total_money")
+    var plusMoney = document.getElementById(""+name+"money")
     if(document.getElementById("food_"+name+"") ==null){
-        // 결제금액 추가
-        document.getElementById("pay_food").value = parseInt(money)
-        var tMoney = document.getElementById("total_money").value
-        tMoney = parseInt(tMoney) + parseInt(money)
-        document.getElementById("total_money").value = tMoney
+        pMoney.value = parseInt(pMoney.value) + parseInt(money)
+        var sumMoney = tMoney.value
+        tMoney.value = parseInt(sumMoney) + parseInt(money)
         // 주문표 표시
+
         document.getElementsByClassName("order-wrap")[0].innerHTML += `
-            <li class="item-list">
+            <li class="item-list" id="order-li">
                 <div>
                     <div class="item-name" id="food_`+name+`">`+name+`</div>
                     <div class="pull-left">
-                        <a class="delete-btn" href="">
+                        <a class="delete-btn" id="`+name+`delete" onclick='deleteDiv(this.id)' style="cursor: pointer;">
                             삭제
                             <span id="`+name+`money">`+ money +`</span><span>원</span>
                         </a>
-                        <div class="text-right">- 1 +</div>
+                        <div class="text-right"><input type="button" value="-"><input type="button" value="1"><input type="button" value="+"></div>
                     </div>
                 </div>
             </li>`
     }
     else{
         console.log("두번째")
-        var pMoney = document.getElementById(""+name+"money").innerText
-        pMoney = parseInt(pMoney)
-        pMoney += parseInt(money)
+        var sMoney = parseInt(plusMoney.innerText)
+        sMoney += parseInt(money)
         // 담기 두번 이상 눌렀을때 기존 금액에 추가
-        document.getElementById(""+name+"money").innerText = pMoney
+        plusMoney.innerText = sMoney
         // 결제 금액에 추가
-        var t1Money = document.getElementById("pay_food").value
-        t1Money = parseInt(t1Money) + parseInt(money)
-        document.getElementById("pay_food").value = t1Money
-        var t2Money = document.getElementById("total_money").value
-        t2Money = parseInt(t2Money) + parseInt(money)
-        document.getElementById("total_money").value = t2Money
+        // 음식비
+        pMoney.value = parseInt(pMoney.value) + parseInt(money)
+        // 총 금액
+        tMoney.value = parseInt(tMoney.value) + parseInt(money)
     }
+}
+
+function deleteDiv(id) {
+    const div = document.getElementById('order-li');
+    console.log( document.getElementsByClassName('item-list'))
+    // div.remove();
 }
