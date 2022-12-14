3d9318c1ee751b93d86baa9f5bd56dc26a612609
whddla <whddla8912@gmail.com>
2022-12-14 17:51:41 +0900

    결제 api 추가
    결제 api 추가

--- src/main/java/com/example/food_delivery/domain/controller/OrderController.java
+++ src/main/java/com/example/food_delivery/domain/controller/OrderController.java
@@ -3,11 +3,19 @@ package com.example.food_delivery.domain.controller;
 import com.example.food_delivery.domain.service.StoreService;
 import com.example.food_delivery.domain.vo.MenuVO;
 import com.example.food_delivery.domain.vo.StoreVO;
+import com.example.food_delivery.domain.vo.UserVO;
+import com.example.food_delivery.web.LoginForm;
+import com.example.food_delivery.web.SessionConstants;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 //@RestController api 확인시 기존 Conroller 어노테이션 주석하고 이거 활성
 @Controller
@@ -18,10 +26,23 @@ public class OrderController {
 
     //주문화면
     @GetMapping("order")
-    public String orderPage(Model model, Integer no, StoreVO storeVO, MenuVO menuVO){
+    public String orderPage(Model model, Integer no, StoreVO storeVO, MenuVO menuVO, HttpServletRequest request){
         model.addAttribute("store",storeService.order(storeVO,no));
         model.addAttribute("menu",storeService.menu(menuVO,no));
         return "user/userOrder";
+    }
+
+    //주문완료
+    @GetMapping("order/wait")
+    public String waitPage(){
+        return "user/wait";
+    }
+    @PostMapping("order/success")
+    @ResponseBody
+    public Object orderSuccess(@RequestBody Map<String, Object> map) {
+        map.put("no", map.get("no"));
+        map.put("money", map.get("money"));
+        return map;
     }
 
 }
