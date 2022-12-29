bc1bc53901806c20a1743ad3838dae569112374e
whddla <whddla8912@gmail.com>
2022-12-29 18:40:03 +0900

    메뉴 개수 추가
    메뉴 개수 추가

---
+++ src/main/resources/static/js/store/order.js
@@ -0,0 +1,11 @@
+function menuCount() {
+    var orderNo = document.getElementsByClassName("select-menu")
+
+    for(var i=0;i<orderNo.length;i++){
+        var menu = orderNo.item(i).getElementsByTagName("span")
+        menuCount = menu.length
+        menu.item(0).innerText = '메뉴 ' + menuCount +'개'
+    }
+}
+
+menuCount();
