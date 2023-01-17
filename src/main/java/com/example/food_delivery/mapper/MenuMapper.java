c0dc0912464f5169871d448fdff07dda45d5f1cf
whddla <whddla8912@gmail.com>
2023-01-17 23:38:01 +0900

    MenuMapper
    메뉴 한개 가져오기 추가

--- src/main/java/com/example/food_delivery/mapper/MenuMapper.java
+++ src/main/java/com/example/food_delivery/mapper/MenuMapper.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Mapper
 public interface MenuMapper {
     public List<MenuVO> menu(MenuVO menuVO, Integer no);
+    public MenuVO selectMenu(Integer no);
+    public MenuVO menuUpdate(MenuVO menuVO, Integer no, String foodName, Integer price, String img);
 }
