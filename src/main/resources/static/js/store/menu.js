9961ca164270084a7f2fc6d664d24edbbe0dd634
whddla <whddla8912@gmail.com>
2023-01-19 21:45:14 +0900

    Menu manage js
    메뉴 이미지 업로드 경로 오류 수정,
    업로드 이미지 경로 추가,
    업로드 이미지 미리보기 추가

--- src/main/resources/static/js/store/menu.js
+++ src/main/resources/static/js/store/menu.js
@@ -1,3 +1,9 @@
+// 가게 이름 찾아오기
+var decodeUri = document.getElementById("menuList").children[1].getElementsByTagName("td")[0].children[0].getAttribute("src")
+var idx =  decodeUri.indexOf("d/")
+decodeUri = decodeUri.substring(idx+2)
+var storeName = decodeUri.substring(0,decodeUri.indexOf("/"))
+
 function showMenuList(no) {
     var json = {"no":no}
     $.ajax({
@@ -40,17 +46,36 @@ function updateMenu(no) {
                          <button onclick="saveMenu(`+no+`)">저장</button>
                          <button onclick="showMenuList(`+no+`)">취소</button>
                      </td>`
+
+    function readImage(input) {
+        // 인풋 태그에 파일이 있는 경우
+        if(input.files && input.files[0]) {
+            // 이미지 파일인지 검사 (생략)
+            // FileReader 인스턴스 생성
+            const reader = new FileReader()
+            // 이미지가 로드가 된 경우
+            reader.onload = e => {
+                const previewImage = document.getElementById("img"+no)
+                previewImage.src = e.target.result
+            }
+            // reader가 이미지 읽도록 하기
+            reader.readAsDataURL(input.files[0])
+        }
+    }
+    // input file에 change 이벤트 부여
+    const inputImage = document.getElementById("uploadImg")
+    inputImage.addEventListener("change", e => {
+        readImage(e.target)
+    })
 }
 
+
 function saveMenu(no) {
-    var decodeUri = decodeURI(document.getElementById("img"+no).src)
-    var idx =  decodeUri.indexOf("img")
-    decodeUri = decodeUri.substring(idx-1)
-    console.log(decodeUri)
-    // console.log(decodeUri)
+    console.log(storeName)
+    var imgUri = "/img/food"+storeName+"/"+document.getElementById("fn"+no).value +".jpg"
     var json = {
         "no" : no,
-        "img": document.getElementById("img"+no).src,
+        "img": imgUri,
         "foodName":document.getElementById("fn"+no).value,
         "price": parseInt(document.getElementById("price"+no).value)
     }
@@ -94,4 +119,5 @@ function deleteMenu(no){
         })
     }
 }
+
 
