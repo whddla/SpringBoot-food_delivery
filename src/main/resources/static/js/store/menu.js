345a6248bdda447dbc167fa6b33c67cdbf081398
whddla <whddla8912@gmail.com>
2023-01-19 20:24:05 +0900

    Menu manage js
    메뉴 삭제 클릭 시 삭제 ajax 추가 (오류 수정 중)
    메뉴 수정 후 저장 클릭 시 ajax 추가 ( 오류 수정 중)

--- src/main/resources/static/js/store/menu.js
+++ src/main/resources/static/js/store/menu.js
@@ -1,17 +1,3 @@
-
-// function updateMenu(e){
-//     const menuList = e.parentElement.parentElement.getElementsByTagName("td")
-//     console.log(e)
-//     console.log(e.nextElementSibling)
-//     console.log(e.parentElement.parentElement.getElementsByTagName("td")[0].children)
-//     menuList[0].innerHTML = `<div></div>`
-//     for(var i=1;i<3;i++){
-//         menuList[i].children[0].removeAttribute("disabled")
-//         menuList[i].children[0].style.border = "2px solid"
-//     }
-//     menuList[2].children[0].value.replace("원",'')
-// }
-
 function showMenuList(no) {
     var json = {"no":no}
     $.ajax({
@@ -21,12 +7,11 @@ function showMenuList(no) {
         dataType:'json',
         contentType: "application/json; charset=UTF-8",
         success: function (result) {
-            console.log(result)
             var htmls = ""
             htmls = `<tr>
-                <td><img style="width: 100px;height: 100px;" src="`+result.img+`"></td>
-                <td><input style="width: 100px;border: none;background-color: white;" disabled type="text" name="" value="`+result.foodName+`"></td>
-                <td><input style="width: 100px;border: none;background-color: white;" disabled type="text" name="" value="`+result.price.toLocaleString()+'원'`"></td>
+                <td><img id="img`+no+`" style="width: 100px;height: 100px;" src="`+result.img+`"></td>
+                <td><input id="fn`+no+`" style="width: 150px;border: none;background-color: white;" disabled type="text" name="" value="`+result.foodName+`"></td>
+                <td><input id="price`+no+`" style="width: 150px;border: none;background-color: white;" disabled type="text" name="" value="`+result.price.toLocaleString()+`원"></td>
                 <td>
                     <button onclick="updateMenu(`+result.no+`)">수정</button>
                     <button onclick="deleteMenu(`+result.no+`)">삭제</button>
@@ -40,33 +25,73 @@ function showMenuList(no) {
 }
 
 function updateMenu(no) {
-    var json = {"no":no}
-    $.ajax({
-        url:"/updateMenu",
-        type:"POST",
-        data:JSON.stringify(json),
-        dataType:'json',
-        contentType: "application/json; charset=UTF-8",
-        success: function (result) {
-            console.log(result)
-            var htmls = ""
-            htmls = `<tr>
-                <td><img style="width: 100px;height: 100px;" src="`+result.img+`"></td>
-                <td><input style="width: 100px;border: 2px solid;background-color: white;" type="text" name="" value="`+result.foodName+`"></td>
-                <td><input style="width: 100px;border: 2px solid;background-color: white;" type="text" name="" value="`+result.price+`"></td>
-                <td>
-                    <button onclick="saveMenu(`+result.no+`)">저장</button>
-                    <button onclick="showMenuList(`+result.no+`)">취소</button>
-                </td>
-             </tr>`
-            $("#"+no)[0].innerHTML = htmls;
-        }, error: function(error){
-            console.log("에러 : " + error);
-        }
-    })
+    var td = $("#"+no)[0].children;
+    for(var i=1;i<3;i++){
+        td[i].children[0].removeAttribute("disabled")
+        td[i].children[0].style.border = "1px solid"
+    }
+    td[2].children[0].value = td[2].children[0].value.replace(",","").replace("원","")
+    td[0].innerHTML = `<label for="uploadImg">
+                            <img id="img`+no+`" src="`+document.getElementById('img'+no).src+`" style="width: 100px;height: 100px; cursor: pointer">
+                       </label>
+                       <input style="display: none" type="file" id="uploadImg">
+                       `
+    td[3].innerHTML = `<td>
+                         <button onclick="saveMenu(`+no+`)">저장</button>
+                         <button onclick="showMenuList(`+no+`)">취소</button>
+                     </td>`
 }
 
-function deleteMenu(){
-    console.log("hi")
+function saveMenu(no) {
+    var decodeUri = decodeURI(document.getElementById("img"+no).src)
+    var idx =  decodeUri.indexOf("img")
+    decodeUri = decodeUri.substring(idx-1)
+    console.log(decodeUri)
+    // console.log(decodeUri)
+    var json = {
+        "no" : no,
+        "img": document.getElementById("img"+no).src,
+        "foodName":document.getElementById("fn"+no).value,
+        "price": parseInt(document.getElementById("price"+no).value)
+    }
+    if(!confirm("메뉴를 저장하시겠습니까?")){
+        alert("취소됐습니다.")
+    }else{
+        $.ajax({
+            url:"/saveMenu",
+            type:"POST",
+            data:JSON.stringify(json),
+            dataType:'json',
+            contentType: "application/json; charset=UTF-8",
+            success: function (result) {
+                  console.log(result)
+            },error: function (err) {
+                console.log(err)
+            }
+        })
+    }
+}
+
+
+function deleteMenu(no){
+    var json = {
+        "no" : no
+    }
+    if(!confirm("메뉴를 삭제하시겠습니까?")){
+        alert("취소됐습니다.")
+    }else{
+        $.ajax({
+            url:"/deleteMenu",
+            type:"POST",
+            data:JSON.stringify(json),
+            dataType:'json',
+            contentType: "application/json; charset=UTF-8",
+            success: function (result) {
+                console.log(result)
+            },error: function (err) {
+                console.log(err)
+            }
+        })
+    }
 }
 
