262ece998af676fa827cc8fa52aa0c37e8473ffb
whddla <whddla8912@gmail.com>
2023-01-21 21:11:18 +0900

    Menu js
    이미지 업로드 삭제

--- src/main/resources/static/js/store/menu.js
+++ src/main/resources/static/js/store/menu.js
@@ -38,47 +38,18 @@ function updateMenu(no) {
         td[i].children[0].style.border = "1px solid"
     }
     td[2].children[0].value = td[2].children[0].value.replace(",","").replace("원","")
-    td[0].innerHTML = `<label for="uploadImg">
-                            <img id="img`+no+`" src="`+document.getElementById('img'+no).src+`" style="width: 100px;height: 100px; cursor: pointer">
-                       </label>
-                       <input style="display: none" type="file" id="uploadImg">
-                       `
     td[3].innerHTML = `<td>
                          <button onclick="saveMenu(`+no+`)">저장</button>
                          <button onclick="showMenuList(`+no+`)">취소</button>
                      </td>`
-
-    function readImage(input) {
-        // 인풋 태그에 파일이 있는 경우
-        if(input.files && input.files[0]) {
-            // 이미지 파일인지 검사 (생략)
-            // FileReader 인스턴스 생성
-            const reader = new FileReader()
-            // 이미지가 로드가 된 경우
-            reader.onload = e => {
-                const previewImage = document.getElementById("img"+no)
-                previewImage.src = e.target.result
-            }
-            // reader가 이미지 읽도록 하기
-            reader.readAsDataURL(input.files[0])
-        }
-    }
-    // input file에 change 이벤트 부여
-    const inputImage = document.getElementById("uploadImg")
-    inputImage.addEventListener("change", e => {
-        readImage(e.target)
-    })
 }
 
 
 function saveMenu(no) {
-    console.log(storeName)
-    var imgUri = "/img/food/"+storeName+"/"+document.getElementById("fn"+no).value +".jpg"
     var json = {
         "no" : no,
-        "img": imgUri,
         "foodName":document.getElementById("fn"+no).value,
-        "price": parseInt(document.getElementById("price"+no).value)
+        "price": parseInt(document.getElementById("price"+no).value),
     }
     if(!confirm("메뉴를 저장하시겠습니까?")){
         alert("취소됐습니다.")
@@ -90,11 +61,12 @@ function saveMenu(no) {
             dataType:'json',
             contentType: "application/json; charset=UTF-8",
             success: function (result) {
-                  showMenuList(no)
+                console.log(result)
             },error: function (err) {
                 console.log(err)
             }
         })
+        showMenuList(no)
     }
 }
 
